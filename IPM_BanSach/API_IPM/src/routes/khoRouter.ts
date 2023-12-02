cbf4b85d1e49eada0b2f7298339e0bad58e11539
luanhy2k2 <luan2k2hy@gmail.com>
2023-12-02 17:39:25 +0700

    edit

--- IPM_BanSach/API_IPM/src/routes/khoRouter.ts
+++
@@ -1,9 +0,0 @@
-import {  Router } from "express";
-import { container } from "tsyringe";
-import { KhoController } from "../controllers/khoController";
-const KhoRouter = Router();
-const ctr = container.resolve(KhoController);
-KhoRouter.get('/getbyid/:id', ctr.GetKhoById.bind(ctr) );
-KhoRouter.get('/getall', ctr.GetAllKho.bind(ctr));
-
-export default KhoRouter;
